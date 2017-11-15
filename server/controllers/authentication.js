14f0619e5b4c29a622ef5a830d6dc112acb8b6f8
Antonio Nicasio <anicasio@artedigital-mx.com>
2017-11-14 16:59:43 -0800

    passport libraries installed

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
 
@@ -28,7 +35,7 @@ exports.signup = function(req, res, next) {
      user.save(function(err) {
          if(err) { return next(err); }
         // Respond to request indicating the user was created
-         res.json(user);
+         res.json({token: tokenForUser(user)});
      });
     
      
