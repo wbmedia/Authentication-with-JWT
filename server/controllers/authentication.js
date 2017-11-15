dd9cdc073f07a10b78c814dee14509897ef4851e
Antonio Nicasio <anicasio@artedigital-mx.com>
2017-11-14 16:27:51 -0800

    first commit

---
+++ server/controllers/authentication.js
@@ -0,0 +1,35 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+
+    // console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    if(!email || !password) {
+        return res.status(422).send({error: 'You must provide email and password'});
+    }
+
+    // see if user with the given email exists
+    User.findOne( { email:email} , function(err, existingUser) {
+        if(err) { return next(err); }
+        // if a user with emails does exist, return an error
+        if(existingUser) {
+            return res.status(422).send({error: 'Email is in use'});
+        }
+    });
+    
+    // if a user with emails does NOT exist, create and save user record
+    const user = new User({ 
+        email: email,
+        password: password
+     });
+
+     user.save(function(err) {
+         if(err) { return next(err); }
+        // Respond to request indicating the user was created
+         res.json(user);
+     });
+    
+     
+}
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
+
+const router = require('./router');
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+
+// Router
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
